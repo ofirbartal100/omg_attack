c81cc52eaad49f4caf217767b02ee659cf6dfbb9
shahafgoren <shahaf.goren@gmail.com>
2022-02-22 15:26:57 +0200

    partial merge with freq_double_vm branch

---
+++ src/models/dct_losses.py
@@ -0,0 +1,34 @@
+import torch
+import torch_dct as dct
+
+class LowFreqPenaltyLoss(torch.nn.Module):
+
+    def __init__(self):
+        super().__init__()
+
+    def forward(self,delta,spatial_shape=(32,32)):
+        if len(spatial_shape) == 2:
+            res = dct.dct_2d(delta)
+            a = spatial_shape[0]//4
+            b = spatial_shape[1]//4
+            return (res[:,:,:a,:b].abs()).mean()
+        
+        else:
+            raise ValueError(f'LowFreqPenaltyLoss not implemented for spatial_shape: {spatial_shape}')
+
+
+class HighFreqPenaltyLoss(torch.nn.Module):
+
+    def __init__(self):
+        super().__init__()
+
+    def forward(self,delta,spatial_shape=(32,32)):
+        if len(spatial_shape) == 2:
+            res = dct.dct_2d(delta)
+            a = spatial_shape[0]//4
+            b = spatial_shape[1]//4
+            parts = 4 * 4
+            return (parts*res.abs().mean() - res[:,:,:a,:b].abs().mean())/(parts)
+        
+        else:
+            raise ValueError(f'HighFreqPenaltyLoss not implemented for spatial_shape: {spatial_shape}')
